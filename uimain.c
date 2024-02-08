ff7be6e4cf521b663c37f383ae01c05c01f61b6c
Alejandro Rangel <alejandrorangel@CrisLaptop.utep.edu>
2024-02-08 12:32:13 -0700

    added arrow instructions and changed font

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Arrow = t, Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Arrow = a, Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -31,7 +31,7 @@ int main()
     case 'c':
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
-	print_char_11x16(c);
+	print_char_5x7(c);
       break;
     case 'q':
       puts("Bye!");
